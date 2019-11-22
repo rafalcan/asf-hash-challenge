7c20ce88aab23f701941f8e92a1fd2296f283a4d
Rafael Gomes Alcantara <rafalcan@gmail.com>
2019-11-22 01:26:56 -0300

    fix component tests and remove the useless test

--- src/app/pages/Home/components/Form.test.js
+++ src/app/pages/Home/components/Form.test.js
@@ -1,8 +1,13 @@
+import StateManager from '@app/services/StateManager';
 import Form from './Form';
 
 describe('Form component tests', () => {
   it('should renders without crashing', () => {
-    const form = Form();
+    const stateManager = new StateManager([
+      { label: 'Label 1', value: '1,00' },
+      { label: 'Label 2', value: '2,00' },
+    ]);
+    const form = Form(stateManager);
     expect(form).toBeTruthy();
   });
 });
