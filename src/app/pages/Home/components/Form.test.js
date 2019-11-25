b4fc3ee98d593fa68067343bce2ca1094caba24a
Rafael Gomes Alcantara <rafalcan@gmail.com>
2019-11-25 04:18:40 -0300

    fix the tests and create new ones

--- src/app/pages/Home/components/Form.test.js
+++ src/app/pages/Home/components/Form.test.js
@@ -1,10 +1,11 @@
-import { createState } from '@app/helpers';
-import StateManager from '@app/services/StateManager';
+import StateManager, { createStateByDays } from '@app/services/StateManager';
 import Form from './Form';
 
 describe('Form component tests', () => {
   it('should renders without crashing', () => {
-    const stateManager = new StateManager(createState([0, 0, 0, 0]));
+    const stateManager = StateManager({
+      items: createStateByDays([0, 0, 0, 0]),
+    });
     expect(Form(stateManager)).toBeTruthy();
   });
 
